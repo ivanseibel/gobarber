6e6f141186d71b6dd7d5cdea8125f3737be87163
Ivan Seibel <ivanseibel@gmail.com>
2020-02-08 18:12:20 -0300

    Adding multipart/form-data with multer.

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
+import multerConfig from './config/multer';
 
 const routes = Router();
+const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
 routes.post('/users', UserController.store);
@@ -12,6 +15,7 @@ routes.post('/users', UserController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+routes.post('/files', upload.single('file'), (req, res) => res.json({ ok: true }));
 
 
 export default routes;
