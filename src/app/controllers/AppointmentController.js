61a39e90be29b9e5072e3fdebc58b05d180cdd01
Ivan Seibel <ivanseibel@gmail.com>
2020-02-14 21:24:45 -0300

    Creating new route to list user appointments and some bug corrections - New route GET /appointments created - New index method created at AppointmentController - Corrected a bug inside AppointmentController.store: user_id writing null on database

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -3,8 +3,35 @@ import { startOfHour, parseISO, isBefore } from 'date-fns';
 
 import User from '../models/User';
 import Appointment from '../models/Appointment';
+import File from '../models/File';
 
 class AppointmentController {
+  async index(req, res) {
+    const appointments = await Appointment.findAll({
+      where: {
+        canceled_at: null,
+        user_id: req.userId,
+      },
+      order: ['date'],
+      attributes: ['id', 'date'],
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'name'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['id', 'path', 'url'],
+            },
+          ],
+        },
+      ],
+    });
+    return res.json(appointments);
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       provider_id: Yup.number().integer().strict().required(),
@@ -47,7 +74,7 @@ class AppointmentController {
     }
 
     const newAppointment = await Appointment.create({
-      user_id: req.user_id,
+      user_id: req.userId,
       provider_id,
       date: hourStart,
     });
