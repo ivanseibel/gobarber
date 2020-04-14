14274b4bfee71b282699f4128c6c6a6e1bb06dd3
Ivan Seibel <ivanseibel@gmail.com>
2020-04-14 14:20:39 -0300

    Add unform component to login and register forms

--- frontend/src/pages/SignUp/index.js
+++ frontend/src/pages/SignUp/index.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="GoBarber" />
 
-      <form>
-        <input placeholder="Full name" />
-        <input type="email" placeholder="Your email" />
-        <input type="password" placeholder="Yout secret password" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Full name" />
+        <Input name="email" type="email" placeholder="Your email" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Yout secret password"
+        />
 
         <button type="submit">Register</button>
         <Link to="/">I already have an account</Link>
-      </form>
+      </Form>
     </>
   );
 }
