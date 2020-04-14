7dec6f5febfabacad648c348181b0b7b38b217ca
Ivan Seibel <ivanseibel@gmail.com>
2020-04-14 09:22:48 -0300

    Adding cors to api

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import 'dotenv/config';
 import express from 'express';
 import path from 'path';
@@ -21,6 +22,7 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use('/files', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')));
   }
