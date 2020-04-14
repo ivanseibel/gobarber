9d7fd430bee5c49f1baf709884329d042e54dcfd
Ivan Seibel <ivanseibel@gmail.com>
2020-04-14 14:01:22 -0300

    Creating styles to login and register pages

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
-  return <h1>SignIn</h1>;
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <form>
+        <input type="email" placeholder="Your email" />
+        <input type="password" placeholder="Yout secret password" />
+
+        <button type="submit">Login</button>
+        <Link to="/register">Create your free account</Link>
+      </form>
+    </>
+  );
 }
