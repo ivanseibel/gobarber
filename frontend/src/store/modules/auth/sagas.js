0a5b4e1dbbfff6b17d70a5dff1419e1bad2303e7
Ivan Seibel <ivanseibel@gmail.com>
2020-04-15 11:00:42 -0300

    Creating new provider register

--- frontend/src/store/modules/auth/sagas.js
+++ frontend/src/store/modules/auth/sagas.js
@@ -31,4 +31,25 @@ export function* signIn({ payload }) {
   }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+    toast.success('Your account was created with success');
+    history.push('/');
+  } catch (error) {
+    toast.error('An error occurred while trying to create your account');
+    yield put(signFailure());
+  }
+}
+
+export default all([
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+]);
