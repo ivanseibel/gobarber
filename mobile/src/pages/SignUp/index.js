40c1674251eda30b8182190dc52156e664626296
Ivan Seibel <ivanseibel@gmail.com>
2020-05-27 14:17:53 -0300

    First commit

---
+++ mobile/src/pages/SignUp/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+import Background from '~/components/Background';
+
+const SignUp = () => {
+  return (
+    <View>
+      <Background>
+        <Text>SignIn</Text>
+      </Background>
+    </View>
+  );
+};
+
+export default SignUp;
