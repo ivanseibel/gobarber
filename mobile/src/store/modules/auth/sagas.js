fd402621a72b63b383f1e1f5cd103310676e25e7
Ivan Seibel <ivanseibel@gmail.com>
2020-06-02 12:16:59 -0300

    Configuring redux+saga+persisting+reactotron

---
+++ mobile/src/store/modules/auth/sagas.js
@@ -0,0 +1,75 @@
+import { Alert } from 'react-native';
+import { takeLatest, all, call, put } from 'redux-saga/effects';
+
+import api from '../../../services/api';
+
+import { signInSuccess, signFailure } from './actions';
+
+export function* signIn({ payload }) {
+  try {
+    const { email, password } = payload;
+
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
+
+    const { token, user } = response.data;
+
+    if (user.provider) {
+      Alert.alert('Login error', 'User cannot be a provider');
+      return;
+    }
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
+
+    yield put(signInSuccess(token, user));
+
+    // history.push('/dashboard');
+  } catch (error) {
+    Alert.alert('Login error', 'Authentication has been failed');
+    yield put(signFailure());
+  }
+}
+
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+    Alert.alert('Success', 'Your account was created with success');
+    // history.push('/');
+  } catch (error) {
+    Alert.alert(
+      'Register error',
+      'An error occurred while trying to create your account'
+    );
+    yield put(signFailure());
+  }
+}
+
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.authorization = `Bearer ${token}`;
+  }
+}
+
+export function signOut() {
+  // history.push('/');
+}
+
+export default all([
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+  takeLatest('@auth/SIGN_OUT', signOut),
+]);
