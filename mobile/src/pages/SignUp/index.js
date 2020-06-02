c1541aec8e11f542b9f10842dcf38307b3ad1195
Ivan Seibel <ivanseibel@gmail.com>
2020-06-02 10:20:52 -0300

    Creating SignUp page

--- mobile/src/pages/SignUp/index.js
+++ mobile/src/pages/SignUp/index.js
@@ -1,17 +1,64 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import PropTypes from 'prop-types';
+import { Image } from 'react-native';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.png';
+
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+
 import Background from '~/components/Background';
 
-const SignUp = () => {
+const SignUp = ({ navigation }) => {
   return (
-    <View>
-      <Background>
-        <Text>SignIn</Text>
-      </Background>
-    </View>
+    <Background>
+      <Container>
+        <Image source={logo} />
+        <Form>
+          <FormInput
+            icon="person-outline"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Full name"
+          />
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Email"
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Password"
+          />
+
+          <SubmitButton onPress={() => {}}>Login</SubmitButton>
+        </Form>
+
+        <SignLink
+          onPress={() => {
+            navigation.navigate('SignUp');
+          }}
+        >
+          <SignLinkText>Create new free account</SignLinkText>
+        </SignLink>
+      </Container>
+    </Background>
   );
+};
+
+SignUp.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired,
+  }).isRequired,
 };
 
 export default SignUp;
