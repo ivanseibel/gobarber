bf7e9d7a341b72df610733b05aa47c7cc1679871
Ivan Seibel <ivanseibel@gmail.com>
2020-06-03 11:22:20 -0300

    Get, display and cancel appointments - Installed date-fns library - Created method to get appointments - Appointment data formatted into Appointment component - Created method to cancel appointments

--- mobile/src/pages/Dashboard/index.js
+++ mobile/src/pages/Dashboard/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { Alert } from 'react-native';
 
 import { Container, Title, List } from './styles';
 
 import Background from '~/components/Background';
 import Appointment from '~/components/Appointment';
+
+import api from '~/services/api';
 
 const Dashboard = ({ navigation }) => {
   navigation.setOptions({
@@ -15,16 +18,51 @@ const Dashboard = ({ navigation }) => {
     },
   });
 
-  const data = [1, 2, 3, 4, 5];
+  const [appointments, setAppointments] = useState([]);
+
+  useEffect(() => {
+    async function loadAppointments() {
+      try {
+        const response = await api.get('appointments');
+        setAppointments(response.data);
+      } catch (error) {
+        Alert.alert(error.message);
+      }
+    }
+
+    loadAppointments();
+  }, []);
+
+  const handleCancel = async (id) => {
+    try {
+      const response = await api.delete(`appointments/${id}`);
+      setAppointments(
+        appointments.map((a) =>
+          a.id === id
+            ? { ...a, canceled_at: response.data.canceled_at }
+            : { ...a }
+        )
+      );
+    } catch (error) {
+      Alert.alert(error.message);
+    }
+  };
 
   return (
     <Background>
       <Container>
         <Title>Appointments</Title>
         <List
-          data={data}
-          keyExtractor={(item) => String(item)}
-          renderItem={({ item }) => <Appointment data={item} />}
+          data={appointments}
+          keyExtractor={(item) => String(item.id)}
+          renderItem={({ item }) => (
+            <Appointment
+              data={item}
+              onCancel={() => {
+                handleCancel(item.id);
+              }}
+            />
+          )}
         />
       </Container>
     </Background>
@@ -32,7 +70,7 @@ const Dashboard = ({ navigation }) => {
 };
 
 Dashboard.propTypes = {
-  navigation: PropTypes.shape.isRequired,
+  navigation: PropTypes.shape().isRequired,
 };
 
 export default Dashboard;
