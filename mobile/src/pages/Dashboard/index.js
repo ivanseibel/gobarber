441f21e1a3b2ce255e8d88e2da3011fed5b4eef3
Ivan Seibel <ivanseibel@gmail.com>
2020-06-03 09:01:07 -0300

    Preparing Dashboard page to receive appointments

--- mobile/src/pages/Dashboard/index.js
+++ mobile/src/pages/Dashboard/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container } from './styles';
+import { Container, Title, List } from './styles';
 
 import Background from '~/components/Background';
+import Appointment from '~/components/Appointment';
 
 const Dashboard = ({ navigation }) => {
   navigation.setOptions({
@@ -15,7 +15,20 @@ const Dashboard = ({ navigation }) => {
     },
   });
 
-  return <Background />;
+  const data = [1, 2, 3, 4, 5];
+
+  return (
+    <Background>
+      <Container>
+        <Title>Appointments</Title>
+        <List
+          data={data}
+          keyExtractor={(item) => String(item)}
+          renderItem={({ item }) => <Appointment data={item} />}
+        />
+      </Container>
+    </Background>
+  );
 };
 
 Dashboard.propTypes = {
