2e83c64a2a8733811c9db43229c6174cc278f56d
Ivan Seibel <ivanseibel@gmail.com>
2020-06-03 12:28:51 -0300

    Creating Profile page - Created basic structure - Created styles - Getted user actual information - Implemented method to update user data

--- mobile/src/pages/Profile/styles.js
+++ mobile/src/pages/Profile/styles.js
@@ -1,3 +1,37 @@
 import styled from 'styled-components/native';
+import Input from '~/components/Input';
+import Button from '~/components/Button';
 
-export const Container = styled.View``;
+export const Container = styled.SafeAreaView`
+  flex: 1;
+`;
+
+export const Title = styled.Text`
+  font-size: 20px;
+  color: #fff;
+  font-weight: bold;
+  align-self: center;
+  margin-top: 30px;
+  margin-bottom: 30px;
+`;
+
+export const Form = styled.ScrollView.attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: { paddingLeft: 30, paddingRight: 30 },
+})`
+  align-self: stretch;
+`;
+
+export const FormInput = styled(Input)`
+  margin-bottom: 10px;
+`;
+
+export const SubmitButton = styled(Button)`
+  margin-top: 5px;
+`;
+
+export const Separator = styled.View`
+  height: 1px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 20px 0 30px;
+`;
