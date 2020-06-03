5464009d5afcf8aa2d8365a8faa7c3187be9dc08
Ivan Seibel <ivanseibel@gmail.com>
2020-06-03 16:56:14 -0300

    Create page SelectProvider

--- mobile/src/pages/New/SelectProvider/index.js
+++ mobile/src/pages/New/SelectProvider/index.js
@@ -1,13 +1,53 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Alert } from 'react-native';
 
-import { Container } from './styles';
+import { Container, ProvidersList, Provider, Avatar, Name } from './styles';
 import Background from '~/components/Background';
 
-const SelectProvider = () => {
+import api from '~/services/api';
+
+const SelectProvider = ({ navigation }) => {
+  const [providers, setProviders] = useState([]);
+
+  useEffect(() => {
+    const loadProviders = async () => {
+      try {
+        const response = await api.get('providers');
+        setProviders(response.data);
+      } catch (error) {
+        Alert.alert(error.message);
+      }
+    };
+    loadProviders();
+  }, []);
+
   return (
     <Background>
-      <Container>{/*  */}</Container>
+      <Container>
+        <ProvidersList
+          data={providers}
+          keyExtractor={(provider) => String(provider.id)}
+          renderItem={({ item: provider, index }) => (
+            <Provider
+              onPress={() => {
+                navigation.navigate('SelectDateTime', { provider });
+              }}
+              last={index === providers.length - 1}
+            >
+              <Avatar
+                source={
+                  provider.avatar
+                    ? { uri: provider.avatar.url }
+                    : {
+                        uri: `https://api.adorable.io/avatars/50/${provider.name}.png`,
+                      }
+                }
+              />
+              <Name>{provider.name}</Name>
+            </Provider>
+          )}
+        />
+      </Container>
     </Background>
   );
 };
--- mobile/src/pages/New/SelectProvider/styles.js
+++ mobile/src/pages/New/SelectProvider/styles.js
@@ -1,3 +1,42 @@
 import styled from 'styled-components/native';
+import { FlatList } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 
-export const Container = styled.View``;
+export const Container = styled.SafeAreaView`
+  flex: 1;
+`;
+
+export const ProvidersList = styled(FlatList).attrs({
+  showVerticalScrollIndicator: false,
+  numColumns: 2,
+})`
+  margin-top: 60px;
+  padding: 0 20px;
+`;
+
+export const Provider = styled(RectButton)`
+  background: #fff;
+  border-radius: 4px;
+  padding: 20px;
+  flex: 0.5;
+
+  align-items: center;
+  justify-content: space-between;
+  margin-right: ${(props) => (props.last ? '20px' : '10px')};
+  margin-left: 10px;
+  margin-bottom: 20px;
+`;
+
+export const Avatar = styled.Image`
+  height: 50px;
+  width: 50px;
+  border-radius: 25px;
+`;
+
+export const Name = styled.Text`
+  margin-top: 15px;
+  font-size: 14px;
+  font-weight: bold;
+  color: #333;
+  text-align: center;
+`;
