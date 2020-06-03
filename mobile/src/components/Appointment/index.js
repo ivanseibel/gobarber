33caca01e62634864d9abf6b333b2e5a7fc61db2
Ivan Seibel <ivanseibel@gmail.com>
2020-06-03 11:22:20 -0300

    Get, display and cancel appointments - Installed date-fns library - Created method to get appointments - Appointment data formatted into Appointment component - Created method to cancel appointments

--- mobile/src/components/Appointment/index.js
+++ mobile/src/components/Appointment/index.js
@@ -1,27 +1,39 @@
-import React from 'react';
-import { TouchableOpacity } from 'react-native';
+import React, { useMemo } from 'react';
+import { parseISO, formatRelative } from 'date-fns';
+import { TouchableOpacity, Alert } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { Container, Left, Avatar, Info, Name, Time } from './styles';
 
-const Appointment = () => {
+const Appointment = ({ data, onCancel }) => {
+  const { provider, date, past, cancelable, id, canceled_at } = data;
+
+  const fakeAvatar = () => {
+    return `https://api.adorable.io/avatars/50/${provider.name}.png`;
+  };
+
+  const dateParsed = useMemo(() => {
+    return formatRelative(parseISO(date), new Date(), { addSuffix: true });
+  }, [date]);
+
   return (
-    <Container>
+    <Container past={past}>
       <Left>
         <Avatar
-          source={{
-            uri: 'https://api.adorable.io/avatars/50/gobarbermobile.png',
-          }}
+          source={{ uri: provider.avatar ? provider.avatar.url : fakeAvatar() }}
         />
 
         <Info>
-          <Name>Ivan Luis Seibel</Name>
-          <Time>in 3 hours</Time>
+          <Name>{provider.name}</Name>
+          <Time>{dateParsed}</Time>
         </Info>
       </Left>
-      <TouchableOpacity onPress={() => {}}>
-        <Icon name="event-busy" size={20} color="#f64c75" />
-      </TouchableOpacity>
+
+      {cancelable && !canceled_at && (
+        <TouchableOpacity onPress={onCancel}>
+          <Icon name="event-busy" size={20} color="#f64c75" />
+        </TouchableOpacity>
+      )}
     </Container>
   );
 };
