bd74c63030301bf915d8b457548ea986ee09bc88
Ivan Seibel <ivanseibel@gmail.com>
2020-06-04 10:05:17 -0300

    Implemented method to select date/time schedule

--- mobile/src/pages/New/SelectDateTime/index.js
+++ mobile/src/pages/New/SelectDateTime/index.js
@@ -1,16 +1,54 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Alert } from 'react-native';
 
-import { Container } from './styles';
+import { Container, HourList, Hour, Title } from './styles';
 import Background from '~/components/Background';
 import DateInput from '~/components/DateInput';
 
-const SelectDateTime = () => {
+import api from '~/services/api';
+
+const SelectDateTime = ({ navigation, route }) => {
+  const [availableTimes, setAvailableTimes] = useState([]);
   const [date, setDate] = useState(new Date());
+  const { provider } = route.params;
+
+  useEffect(() => {
+    const loadTimes = async () => {
+      try {
+        const response = await api.get(`providers/${provider.id}/available`, {
+          params: { date: date.getTime() },
+        });
+        setAvailableTimes(response.data);
+      } catch (error) {
+        Alert.alert(error.message);
+      }
+    };
+    loadTimes();
+  }, [date]);
+
+  const handleSelectTime = (time) => {
+    navigation.navigate('Confirm', {
+      provider,
+      time,
+    });
+  };
 
   return (
     <Background>
       <Container>
         <DateInput date={date} onChange={setDate} />
+        <HourList
+          data={availableTimes}
+          keyExtractor={(item) => item.time}
+          renderItem={({ item }) => (
+            <Hour
+              onPress={() => handleSelectTime(item.value)}
+              enabled={item.available}
+            >
+              <Title>{item.time}</Title>
+            </Hour>
+          )}
+        />
       </Container>
     </Background>
   );
